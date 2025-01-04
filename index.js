a11bf1754801b9989bfa3bd43d51fe98ea34b048
buczekmi <74563947+49M@users.noreply.github.com>
2025-01-03 23:00:09 -0500

    deploy

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const inputBtn = document.getElementById('input-btn');
 const inputEl = document.getElementById('input-el');
 const ulEl = document.getElementById('ul-el');
 const deleteBtn = document.getElementById('delete-btn');
+const tabBtn = document.getElementById('tab-btn');
 
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
 if (leadsFromLocalStorage) {
@@ -40,3 +41,10 @@ deleteBtn.addEventListener("click", function() {
     renderLeads(myLeads);
 });
 
+tabBtn.addEventListener('click', function() {
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {;
+        myLeads.push(tabs[0].url);
+        localStorage.setItem('myLeads', JSON.stringify(myLeads));
+        renderLeads(myLeads);
+    })
+});
