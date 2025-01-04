7eb445c3604ae27ffad267cf94c5b3472d1204da
buczekmi <74563947+49M@users.noreply.github.com>
2025-01-04 09:27:03 -0500

    db save and delete working

--- index.js
+++ index.js
@@ -1,50 +1,52 @@
-let myLeads = [];
-const inputBtn = document.getElementById('input-btn');
-const inputEl = document.getElementById('input-el');
-const ulEl = document.getElementById('ul-el');
-const deleteBtn = document.getElementById('delete-btn');
-const tabBtn = document.getElementById('tab-btn');
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { getDatabase,
+         ref,
+         push,
+         onValue,
+         remove } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-database.js";
 
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage;
-    renderLeads(myLeads);
+const firebaseConfig = { 
+    databaseURL: "https://leads-tracker-13686-default-rtdb.firebaseio.com/"
 }
 
-inputBtn.addEventListener('click', function() {
-    myLeads.push(inputEl.value);
-    localStorage.setItem('myLeads', JSON.stringify(myLeads));
-    renderLeads(myLeads);
-    inputEl.value = '';
-});
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const referenceInDB = ref(database, "leads")
 
-function renderLeads(leads) {
-    let listItems = '';
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+
+function render(leads) {
+    let listItems = ""
     for (let i = 0; i < leads.length; i++) {
-       // listItems += "<li><a target='_blank' href='"+myLeads[i]+"'>"+myLeads[i]+"</a></li>";
-       listItems += `
-       <li>
-         <a target='_blank' href='${leads[i]}'>${leads[i]}</a>
-       </li>
-       ` 
-       // Alternate
-        // const li = document.createElement('li');
-        // li.textContent = myLeads[i];
-        // ulEl.append(li);
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
     }
-    ulEl.innerHTML = listItems;
+    ulEl.innerHTML = listItems
 }
 
 deleteBtn.addEventListener("click", function() {
-    localStorage.clear();
-    myLeads = [];
-    renderLeads(myLeads);
-});
+    remove(referenceInDB);
+    ulEl.innerHTML = ""
+})
 
-tabBtn.addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {;
-        myLeads.push(tabs[0].url);
-        localStorage.setItem('myLeads', JSON.stringify(myLeads));
-        renderLeads(myLeads);
-    })
-});
+inputBtn.addEventListener("click", function() {
+    push(referenceInDB, inputEl.value)
+    inputEl.value = ""
+})
+
+onValue(referenceInDB, function(snapshot) {
+    if (snapshot.exists()) {
+        const snapshotValues = snapshot.val();
+        const leads = Object.values(snapshotValues);
+        render(leads)
+    }
+})
+
